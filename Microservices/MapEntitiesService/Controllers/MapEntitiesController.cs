0c7dbeebc5b2b0349a3dca7badbff53d2f1ef7f8
BakPeter <bak.pet@gmail.com>
2022-05-18 09:57:07 +0300

    Logger files deleted

---
+++ Microservices/MapEntitiesService/Controllers/MapEntitiesController.cs
@@ -0,0 +1,36 @@
+﻿using MapEntitiesService.Core.Model;
+using MapEntitiesService.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapEntitiesService.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MapEntitiesController : Controller
+    {
+        private readonly IMapEntityService _mapEntityService;
+
+        public MapEntitiesController(IMapEntityService mapEntityService)
+        {
+            _mapEntityService = mapEntityService;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<string>> HandleEntity([FromBody] Entity entity)
+        {
+            var result = await _mapEntityService.HandleEntityAsync(entity);
+
+            return Ok(result);
+            //if (result == true)
+            //{
+            //    return Ok(result);
+            //}
+            //else
+            //{
+            //    return StatusCode(StatusCodes.Status500InternalServerError, result);
+            //}
+        }
+    }
+}
