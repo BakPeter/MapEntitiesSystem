6330f088d69b246470bee5687c2d3ac19242106e
Yaniv Ben David <yanivbd@gmail.com>
2022-05-31 17:08:47 +0300

    Added file validator

--- Microservices/MapsRepositoryService.Core/Validation/UploadMapValidation.cs
+++ Microservices/MapsRepositoryService.Core/Validation/UploadMapValidation.cs
@@ -5,19 +5,8 @@ namespace MapsRepositoryService.Core.Validation;
 
 public class UploadMapValidation : IUploadMapValidation
 {
-    //File is not empty
-
-    //File size is not over 1mb
-    //    File name(our file name) will not be empty(field required)
-
-    //Indication on each validation(1st one) -> print it
-    //File name should be unique(check in MinIo maps names) [Last]
-    //File name should be valid(letters and numbers) [REGEX]
-    //Valid extensions.jpeg, .png, .jpg, .svg
-    //    All validations should be in core / validations
-
     public ResultModel Validate(MapModel mapModel)
     {
-        throw new NotImplementedException();
+       
     }
 }
--- Microservices/MapsRepositoryService.Core/Validation/Validators/Interfaces/IFileValidator.cs
+++ Microservices/MapsRepositoryService.Core/Validation/Validators/Interfaces/IFileValidator.cs
@@ -1,6 +1,8 @@
-﻿namespace MapsRepositoryService.Core.Validation.Validators.Interfaces;
+﻿using MapsRepositoryService.Core.Model;
+
+namespace MapsRepositoryService.Core.Validation.Validators.Interfaces;
 
 public interface IFileValidator
 {
-    
+    ResultModel Validate(MapModel mapModel);
 }
