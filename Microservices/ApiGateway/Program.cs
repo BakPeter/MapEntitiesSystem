2adde08e3669895bc1d886f6f80f688537f5455c
Peter Bakshaev <bak.pet@gmail.com>
2022-06-25 14:15:35 -0700

    Merged from remote

--- Microservices/ApiGateway/Program.cs
+++ Microservices/ApiGateway/Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("Ocelot.json");
 
 builder.Services.AddOcelot();
-<<<<<<< HEAD
-=======
 builder.Services.AddCors();
 
 builder.Services.AddCors(options =>
@@ -19,7 +17,6 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader()
     );
 });
->>>>>>> 4e772cfaf017ea32c9c9aebaee86be71a30c2c8d
 
 var app = builder.Build();
 app.UseWebSockets();
